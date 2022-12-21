95b4c7e39902bf277e99283a6ebe5d34b803a948
vsevo <vdexpert11@yandex.ru>
2022-12-22 02:08:34 +0500

    fixed Task_1

--- Task_1.py
+++ Task_1.py
@@ -4,12 +4,13 @@ class ChangeDir:
 
     def __init__(self, set_dir):
         self.set_dir = set_dir
+        self.first_dir = os.path.abspath(os.curdir)
 
     def __enter__(self):
         return os.chdir(self.set_dir)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(r"C:\Users\vsevo\PycharmProjects\homework-36d")
+        os.chdir(rf"{self.first_dir}")
 
 with ChangeDir('dir1'):
     print(os.listdir())
